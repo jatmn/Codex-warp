67d93bcec6f1fc62bf7a1ae94a12ef7b4e426c35
jatmn <the@jat.mn>
2026-08-21 08:18:39 -0700

    style(tool_markup): format classifier test

--- src/tool_markup.rs
+++ src/tool_markup.rs
@@ -79,7 +79,10 @@ mod tests {
 
     #[test]
     fn recognized_tags_are_case_insensitive_and_require_a_tag_boundary() {
-        assert_eq!(recognized_tag("<FUNCTION_CALL name=\"run\">"), Some("function_call"));
+        assert_eq!(
+            recognized_tag("<FUNCTION_CALL name=\"run\">"),
+            Some("function_call")
+        );
         assert_eq!(recognized_tag("<parameter/>"), Some("parameter"));
         assert_eq!(recognized_tag("<toolbox>"), None);
         assert_eq!(recognized_tag("<functionality>"), None);
